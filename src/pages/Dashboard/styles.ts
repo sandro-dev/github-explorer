d9465e11393e48ea59adcfaaa1d47af7a83b5185
Sandro Santos <sandro.dev@gmail.com>
2020-07-11 19:08:43 -0300

    Dashboard initial styles

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 56px;
+  color: #3A3A3A;
+`;
